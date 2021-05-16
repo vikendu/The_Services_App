c273e5e2148f91c992c97e902c1ad2b6407ccdbf
vikendu <vikendu@gmail.com>
2021-05-16 19:34:43 +0530

    cleaned up some dumbassery

--- app/src/main/java/com/vikendu/theservicesapp/AdCreationActivity.java
+++ app/src/main/java/com/vikendu/theservicesapp/AdCreationActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AdCreationTool extends AppCompatActivity {
+public class AdCreationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
