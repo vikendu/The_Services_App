77f10ea616722a74bb45fb7051a2d523d72340fe
vikendu <vikendu@gmail.com>
2021-05-23 19:22:48 +0530

    clean up before we hit DB with Ad data

--- app/src/main/java/com/vikendu/theservicesapp/AdCreationActivity.java
+++ app/src/main/java/com/vikendu/theservicesapp/AdCreationActivity.java
@@ -1,8 +1,10 @@
 package com.vikendu.theservicesapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
@@ -48,11 +50,12 @@ public class AdCreationActivity extends AppCompatActivity {
         adStarRatingPreview = findViewById(R.id.idProviderRating);
     }
 
+    //TODO: since this BS returns wrong value on the first try -> if taking it off Main doesn't work put it in onCreate()
     private double getStarRating(FirebaseUser user){
         String uid;
         if (user != null) {
             uid = user.getUid();
-
+            //TODO: Do the following away from the Main thread
             mDatabase.child(uid).child("rating").get().addOnCompleteListener(task -> {
                 if (!task.isSuccessful()) {
                     Log.e("firebase", "Error getting data", task.getException());
@@ -63,15 +66,24 @@ public class AdCreationActivity extends AppCompatActivity {
             });
         } else {
             //TODO: Create an alert dialog telling the MF to log in
+            //(user != null) tells us the mf is logged in
         }
         return rating;
     }
 
     public void showAdPreview(View view) {
+        hideKeyBoard(this, mPaisa);
+
         adTagLinePreview.setText(getString(mTagLine));
         adDescriptionPreview.setText(getString(mAdDescription));
         adPricePreview.setText(getString(mPaisa));
         adStarRatingPreview.setText(Double.toString(getStarRating(mFirebaseuser)));
+    }
+
+    private void hideKeyBoard(Context context, TextView textView) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(textView.getWindowToken(), 0);
     }
 
     private String getString(AutoCompleteTextView acTextView){
