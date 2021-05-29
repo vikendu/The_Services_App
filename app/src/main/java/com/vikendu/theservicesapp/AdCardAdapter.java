9b86b483972be506cd1de0f8c0bc6ca534cef40a
vikendu <vikendu@gmail.com>
2021-05-29 21:27:32 +0530

    AdCardAdapter.java refactored to provide same functionality as before.

--- app/src/main/java/com/vikendu/theservicesapp/AdCardAdapter.java
+++ app/src/main/java/com/vikendu/theservicesapp/AdCardAdapter.java
@@ -1,6 +1,7 @@
 package com.vikendu.theservicesapp;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class AdCardAdapter extends RecyclerView.Adapter<AdCardAdapter.Viewholder> {
@@ -16,6 +25,9 @@ public class AdCardAdapter extends RecyclerView.Adapter<AdCardAdapter.Viewholder
     // TODO: This whole file needs to be rewritten according to the new Advert.java
     private Context context;
     private ArrayList<Advert> advertArrayList;
+
+    private DatabaseReference mDatabaseProviderRef;
+    private ServiceProvider serviceProvider;
 
     public AdCardAdapter(Context context, ArrayList<Advert> advertArrayList){
         this.context = context;
@@ -27,6 +39,7 @@ public class AdCardAdapter extends RecyclerView.Adapter<AdCardAdapter.Viewholder
     public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_ad_card_format, parent, false);
         return new Viewholder(view);
+
     }
 
     @Override
@@ -36,7 +49,30 @@ public class AdCardAdapter extends RecyclerView.Adapter<AdCardAdapter.Viewholder
         holder.adTagLine.setText(model.getTagLine());
         holder.adDesc.setText(model.getAdDescription());
         holder.adPrice.setText(model.getAdPrice());
-        //holder.providersRating.setText(Float.toString(model.getProvidersRating()));
+        holder.providersRating.setText(serviceProvider.getRating());
+    }
+
+    private void getServiceProvider(ServiceProviderCallback callback) {
+        FirebaseUser mFirebaseuser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = null;
+
+        if(mFirebaseuser != null) {
+            uid = mFirebaseuser.getUid();
+        } else {
+            // TODO: get the mf out of here
+        }
+        mDatabaseProviderRef = FirebaseDatabase.getInstance("https://the-services-app-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("providers");
+        ValueEventListener serviceProviderListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.onCallBack(dataSnapshot.getValue(ServiceProvider.class));
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("On DataChange Listener", "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mDatabaseProviderRef.child(uid).addValueEventListener(serviceProviderListener);
     }
 
     @Override
@@ -52,6 +88,8 @@ public class AdCardAdapter extends RecyclerView.Adapter<AdCardAdapter.Viewholder
             adDesc = itemView.findViewById((R.id.idAdDesc));
             adPrice = itemView.findViewById((R.id.idAdPrice));
             providersRating = itemView.findViewById(R.id.idProviderRating);
+
+            getServiceProvider(value -> serviceProvider = value);
         }
     }
 }
