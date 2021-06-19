41d0404bbd7a06b16debfd9159f49cfc845557bd
vikendu <vikendu@gmail.com>
2021-06-19 23:10:27 +0530

    [Design] FeedActivity.java & layout_location_input.xml created

---
+++ app/src/main/java/com/vikendu/theservicesapp/activity/FeedActivity.java
@@ -0,0 +1,16 @@
+package com.vikendu.theservicesapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vikendu.theservicesapp.R;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
