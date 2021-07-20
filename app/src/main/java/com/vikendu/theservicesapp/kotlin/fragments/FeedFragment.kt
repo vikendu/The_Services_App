be620b346eac4bb5769f5268682e5b86c88ddcbc
vikendu <vikendu@gmail.com>
2021-07-20 19:59:26 +0530

    [MVVM] Restored full functionality for Kotlin feed.

--- app/src/main/java/com/vikendu/theservicesapp/kotlin/fragments/FeedFragment.kt
+++ app/src/main/java/com/vikendu/theservicesapp/kotlin/fragments/FeedFragment.kt
@@ -1,6 +1,8 @@
 package com.vikendu.theservicesapp.kotlin.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,20 +10,49 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vikendu.theservicesapp.R
+import com.vikendu.theservicesapp.activities.ProviderDetailsActivity
 import com.vikendu.theservicesapp.adapters.AdCardAdapter
 import com.vikendu.theservicesapp.models.Advert
+import com.vikendu.theservicesapp.utils.RecyclerItemClickListener
 import com.vikendu.theservicesapp.viewmodels.FeedViewModel
 import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
     private val feedViewModel = FeedViewModel()
+    private var advertArray: ArrayList<Advert> = ArrayList()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        feedViewModel.feedAds.observe(viewLifecycleOwner, Observer {
-            updateFeed(it as ArrayList<Advert>)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        feedViewModel.feedAds.observe(viewLifecycleOwner, {
+            advertArray = it as ArrayList<Advert>
+            updateFeed(it)
         })
         return super.onCreateView(inflater, container, savedInstanceState)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        idRVAdCreation.addOnItemTouchListener(RecyclerItemClickListener(
+            context,
+            idRVAdCreation,
+            object : RecyclerItemClickListener.OnItemClickListener {
+                override fun onItemClick(view: View?, position: Int) {
+                    Log.d("Language", "this is kotlin")
+                    val intent = Intent(view?.context, ProviderDetailsActivity::class.java)
+                    intent.putExtra("advert", advertArray[position])
+                    startActivity(intent)
+                }
+
+                override fun onLongItemClick(view: View?, position: Int) {
+                    TODO("Add option to report and what not")
+                }
+
+            }))
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     private fun updateFeed(advertList: ArrayList<Advert>) {
         val adCardAdapter = AdCardAdapter(context, advertList)
         val linearLayoutManager = LinearLayoutManager(context)
@@ -29,5 +60,9 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
         idRVAdCreation?.adapter = adCardAdapter
     }
 
+    override fun onDestroy() {
+        feedViewModel.removeListener()
+        super.onDestroy()
+    }
     //TODO: Add a listener to go to ProvidersDetailsActivity when AD is touched
 }
--- app/src/main/java/com/vikendu/theservicesapp/viewmodels/FeedViewModel.java
+++ app/src/main/java/com/vikendu/theservicesapp/viewmodels/FeedViewModel.java
@@ -1,26 +1,40 @@
 package com.vikendu.theservicesapp.viewmodels;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.vikendu.theservicesapp.activities.ProviderDetailsActivity;
 import com.vikendu.theservicesapp.models.Advert;
-import com.vikendu.theservicesapp.repos.FeedRepository;
+import com.vikendu.theservicesapp.repos.FeedRepo;
 
 import java.util.List;
 
 public class FeedViewModel extends ViewModel {
-    private FeedRepository feedRepository;
+    private FeedRepo feedRepo;
     private MutableLiveData<List<Advert>> mutableLiveData;
 
     public FeedViewModel() {
-        feedRepository = new FeedRepository();
+        feedRepo = new FeedRepo();
     }
 
     public LiveData<List<Advert>> getFeedAds() {
         if(mutableLiveData == null) {
-            mutableLiveData = feedRepository.requestFeedAdverts();
+            mutableLiveData = feedRepo.requestFeedAdverts();
         }
         return mutableLiveData;
+    }
+
+    public void removeListener() {
+        feedRepo.removeListener();
+    }
+
+    public void showProviderDetails(Context context, Advert ad) {
+        Intent intent = new Intent(context, ProviderDetailsActivity.class);
+        intent.putExtra("advert", ad);
+        context.startActivity(intent);
     }
 }
