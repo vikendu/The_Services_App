605f30610f0473a5045c02292437679a9361dc1d
vikendu <vikendu@gmail.com>
2021-08-03 21:21:02 +0530

    [#128] Search touch listeners.

--- app/src/main/java/com/vikendu/theservicesapp/kotlin/activities/ui/buyers/search/SearchFragment.kt
+++ app/src/main/java/com/vikendu/theservicesapp/kotlin/activities/ui/buyers/search/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.vikendu.theservicesapp.kotlin.activities.ui.buyers.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,9 +9,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vikendu.theservicesapp.R
+import com.vikendu.theservicesapp.activities.ProviderDetailsActivity
 import com.vikendu.theservicesapp.adapters.AdCardAdapter
 import com.vikendu.theservicesapp.databinding.FragmentSearchBinding
 import com.vikendu.theservicesapp.models.Advert
+import com.vikendu.theservicesapp.utils.RecyclerItemClickListener
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
     private var _binding: FragmentSearchBinding? = null
@@ -43,6 +46,27 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.idRVAdCreation.addOnItemTouchListener(
+            RecyclerItemClickListener(
+            context,
+            binding.idRVAdCreation,
+            object : RecyclerItemClickListener.OnItemClickListener {
+                override fun onItemClick(view: View?, position: Int) {
+                    val intent = Intent(view?.context, ProviderDetailsActivity::class.java)
+                    intent.putExtra("advert", allResults[position])
+                    startActivity(intent)
+                }
+
+                override fun onLongItemClick(view: View?, position: Int) {
+                }
+
+            })
+        )
     }
 
     private fun updateFeed(advertList: ArrayList<Advert>) {
