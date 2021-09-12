276f5d9c75f2ad8dcd424ae69fba7a16c9c5ba7c
vikendu <vikendu@gmail.com>
2021-09-12 10:56:35 +0530

    [#58] To be completed.

--- app/src/main/java/com/vikendu/theservicesapp/kotlin/activities/ui/buyers/feed/FeedFragment.kt
+++ app/src/main/java/com/vikendu/theservicesapp/kotlin/activities/ui/buyers/feed/FeedFragment.kt
@@ -3,6 +3,7 @@ package com.vikendu.theservicesapp.kotlin.activities.ui.buyers.feed
 import android.Manifest
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -32,6 +33,7 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
     private val feedViewModel = FeedViewModel()
     private var advertArray: ArrayList<Advert> = ArrayList()
 
+    //LOCATION CODE BEGIN
     private val REQUEST_CODE = 100
     private val MIN_TIME: Float = 5000.0F
     private val MIN_DISTANCE: Long = 1000
@@ -39,29 +41,44 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
 
-    lateinit var locationManager: LocationManager
-
-    private val requestMultiplePermissions =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            permissions.entries.forEach {
-                Log.d(TAG, "${it.key} = ${it.value}")
-            }
-            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true &&
-                permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
-                Log.d(TAG, "Permission granted")
-            } else {
-                Log.d(TAG, "Permission not granted")
-            }
+    private var locationManager : LocationManager? = null
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("L Latitude", location.latitude.toString())
+            Log.d("L Longitude", location.longitude.toString())
         }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+    //LOCATION CODE END
+
+//    private val requestMultiplePermissions =
+//        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+//            permissions.entries.forEach {
+//                Log.d(TAG, "${it.key} = ${it.value}")
+//            }
+//            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true &&
+//                permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+//                Log.d("Permission Status:", "Permission granted")
+//            } else {
+//                Log.d("Permission Status:", "Permission not granted")
+//            }
+//        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if(ActivityCompat.checkSelfPermission(requireContext(),
+        //LOCATION CODE BEGIN
+        if(ActivityCompat.checkSelfPermission
+                (requireContext(),
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(requireActivity(),
                         arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
                         REQUEST_CODE)
         }
+        locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager?
+        //LOCATION CODE END
+
         super.onCreate(savedInstanceState)
     }
 
@@ -99,12 +116,20 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
                 }
 
             }))
+
+        //LOCATION CODE BEGIN
+        binding.locationHeader.refreshButton.setOnClickListener{view ->
+        try {
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_DISTANCE, MIN_TIME, locationListener)
+        } catch (ex: SecurityException){
+            Log.d("SecurityException", "Security Exception, no location available")
+        }}
+        //LOCATION CODE END
         super.onViewCreated(view, savedInstanceState)
     }
 
     private fun updateFeed(advertList: ArrayList<Advert>) {
         //TODO: temporarily updating location from here
-        getCurrentLocation()
         Log.d("Latitude", latitude.toString())
         Log.d("Longitude", longitude.toString())
 
@@ -114,54 +139,60 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
         binding.idRVAdCreation.adapter = adCardAdapter
     }
 
-    private fun getCurrentLocation() {
-        locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        var gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        //var networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+//    private fun getCurrentLocation() {
+//        locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        var gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+//        //var networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+//
+//        if (!gpsEnabled) {
+//            Log.d("GPS Status:", gpsEnabled.toString())
+//            if (ActivityCompat.checkSelfPermission(
+//                    requireContext(),
+//                    Manifest.permission.ACCESS_FINE_LOCATION
+//                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                    requireContext(),
+//                    Manifest.permission.ACCESS_COARSE_LOCATION
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                Log.d("Requesting Permission:", "true")
+//                ActivityCompat.requestPermissions(requireActivity(),
+//                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+//                                Manifest.permission.ACCESS_COARSE_LOCATION),
+//                        REQUEST_CODE)
+//                //TODO: onRequestPermissionResult is deprecated;
+//                // implementing registerForActivityResult [#58] [https://github.com/vikendu/The_Services_App/issues/58#issuecomment-908874144]
+//
+//                registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+//                    permissions.entries.forEach {
+//                        Log.d(TAG, "${it.key} = ${it.value}")
+//                    }
+//                    if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true &&
+//                        permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+//                        Log.d("Permission Status:", "Permission granted")
+//                    } else {
+//                        Log.d("Permission Status:", "Permission not granted")
+//                    }
+//                }
+//            }
+//            locationManager.requestLocationUpdates(
+//                LocationManager.NETWORK_PROVIDER,
+//                MIN_DISTANCE,
+//                MIN_TIME,
+//                locationListener)
+//        }
+//    }
 
-        if (gpsEnabled) {
-            if (ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(requireActivity(),
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.ACCESS_COARSE_LOCATION),
-                        REQUEST_CODE)
-                //TODO: onRequestPermissionResult is deprecated;
-                // implementing registerForActivityResult [#58] [https://github.com/vikendu/The_Services_App/issues/58#issuecomment-908874144]
-                checkPermissions()
-                return
-            }
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                MIN_DISTANCE,
-                MIN_TIME,
-                object : LocationListener {
-                    override fun onLocationChanged(p0: Location) {
-                        //TODO: ("Not yet implemented")
-                        longitude = p0.longitude
-                        latitude =  p0.latitude
-                    }
-                })
-        }
-    }
-
-    private fun checkPermissions() {
-        if (requireContext().let {
-                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION)
-            } != PackageManager.PERMISSION_GRANTED) {
-            //Log.d(TAG, "Request Permissions")
-            requestMultiplePermissions.launch(
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
-        } else {
-            //Log.d(TAG, "Permission Already Granted")
-        }
-    }
+//    private fun checkPermissions() {
+//        if (requireContext().let {
+//                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION)
+//            } != PackageManager.PERMISSION_GRANTED) {
+//            //Log.d(TAG, "Request Permissions")
+//            requestMultiplePermissions.launch(
+//                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
+//        } else {
+//            //Log.d(TAG, "Permission Already Granted")
+//        }
+//    }
 
     override fun onDestroyView() {
         _binding = null
